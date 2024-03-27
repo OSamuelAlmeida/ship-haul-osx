de4fe4c3f7f7c4a8d40a2d78f751c093e2ff3fb8
Samuel Almeida <samuel@bravion.dev>
2024-03-27 14:41:38 -0300

    Initial code setup

---
+++ ShipHaulOSX/ShipHaulOSX/ShipHaulOSXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipHaulOSXApp.swift
+//  ShipHaulOSX
+//
+//  Created by Samuel Almeida on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShipHaulOSXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
